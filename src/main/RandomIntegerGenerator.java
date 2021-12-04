9d41c2e147151a973d24655b738adb1a688d641b
Menhajuddin Sharaf <76524989+menhaj007@users.noreply.github.com>
2021-12-04 01:47:53 -0500

    Update RandomIntegerGenerator.java

--- src/main/RandomIntegerGenerator.java
+++ src/main/RandomIntegerGenerator.java
@@ -8,33 +8,11 @@ public class RandomIntegerGenerator {
         int number = 0, bitsLeft, bitsUsed, requestsLeft;
         int[] arrayOfRandomNNumbers = new int[n];
         try {
-            // checked exception InvalidResponseException
             RandomNumber rn = new RandomNumber("a31090c0-5226-4954-874e-ea1607631855");
             rn.generate(n, max, min, replacement,method);
-
-            // unchecked exception InvalidMethodCallException
             System.out.println("Obtained random numbers:");
-
-            // obtaining random numbers one at a time using getElementAt(int i) function
-//            System.out.println("obtaining random numbers one at a time using getElementAt(int i) function");
-//            for (int i = 0; i< n ; i++)
-//            {
-//                number = rn.getElementAt(i);
-//                System.out.print(number+"\t");
-//            }
-//            System.out.println();
-
-            // obtaining random numbers together as an integer array
             System.out.println("obtaining random numbers together as an integer array");
             arrayOfRandomNNumbers = rn.getElementAsArray();
-//            for (int i = 0; i < n ; i++)
-//            {
-//                number = arrayOfRandomNNumbers[i];
-//                System.out.print(number+"\t");
-//            }
-//            System.out.println();
-
-            // obtaining other metadata about the request
             bitsLeft=rn.getBitsLeft();
             bitsUsed=rn.getBitsUsed();
             requestsLeft=rn.getRequestsLeft();
@@ -52,7 +30,6 @@ public class RandomIntegerGenerator {
             System.out.println(invalidMethodCallException.getMessage());
             invalidMethodCallException.printStackTrace();
         }
-
         return arrayOfRandomNNumbers;
     }
 }
