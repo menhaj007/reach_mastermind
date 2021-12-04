a0c3239c4cd54454c0569144024f8c385fb316e1
Menhajuddin Sharaf <76524989+menhaj007@users.noreply.github.com>
2021-12-04 01:29:23 -0500

    Update MasterMindTest.java

--- src/test/MasterMindTest.java
+++ src/test/MasterMindTest.java
@@ -1,17 +1,12 @@
 package test;
 
 import main.MasterMind;
+import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MasterMindTest {
 
-
-    @Test
-    void name() {
-    }
-
-//    @BeforeEach
     @Test
     void calculate() {
         MasterMind masterMind = new MasterMind();
@@ -20,40 +15,58 @@ class MasterMindTest {
         int expected = 4;
         Assertions.assertEquals(4, masterMind.calculate(tmpArr, tmpArr2));
     }
+    @Test
+    void chooseDifficultyLevel() {
+        MasterMind mm = new MasterMind();
+        //in the chooseDifficultyLevel, change the user choice manually.
+        int userChoice = 2;
+        switch (userChoice) {
+            case 1:
+                System.out.println("If input 1, output should 4");
+                Assertions.assertEquals(4, mm.chooseDifficultyLevel());
+                break;
+            case 2:
+                System.out.println("If input 2, output should 6");
+                Assertions.assertEquals(6, mm.chooseDifficultyLevel());
+                break;
+            case 3:
+                System.out.println("If input 3, output should 8");
+                Assertions.assertEquals(8, mm.chooseDifficultyLevel());
+                break;
+            default:
+                System.out.println("Invalid");
+                break;
+        }
+    }
+    @Test
+    void generateRandomNumbers() {
+        MasterMind masterMind = new MasterMind();
+        System.out.println("Default size of Array is 4. Other choices depend on chooseDifficultyLevel()");
+        Assertions.assertEquals(4, masterMind.generateRandomNumbers().length);
+    }
+    @Test
+    void convertStringToArrayOfInteger() {
+        MasterMind masterMind = new MasterMind();
+        String userGuess1 = "(-1)24";
+        String userGuess2 = "12342347477373";
+        String userGuess3 = "1234";
+        int[] correctAnswer = {1,2,3,4};
+        int correctLength = correctAnswer.length;
+        Assertions.assertEquals(correctAnswer.length, userGuess3.length());
+    }
+    @Test
+    void result() {
+        MasterMind masterMind = new MasterMind();
+        Assertions.assertEquals("You Won! in " + 4 + " attempts", masterMind.result(4));
+    }
+    @Test
+    void hint() {
+        MasterMind masterMind = new MasterMind();
+        int[] randNumbers = {1,2,3,4};
+        int min = randNumbers[0];
+        int max = randNumbers[3];
+        int[] res = {1,4};
+        Assertions.assertArrayEquals(res, masterMind.hint(randNumbers));
+    }
 
-//    @org.junit.jupiter.api.Test
-//    void startGame() {
-//    }
-//
-//    @org.junit.jupiter.api.Test
-//    void chooseDifficultyLevel() {
-//    }
-//
-//    @org.junit.jupiter.api.Test
-//    void generateRandomNumbers() {
-//    }
-//
-//    @org.junit.jupiter.api.Test
-//    void isValid() {
-//    }
-//
-//    @org.junit.jupiter.api.Test
-//    void readUserInput() {
-//    }
-//
-//    @org.junit.jupiter.api.Test
-//    void play() {
-//    }
-//
-//    @org.junit.jupiter.api.Test
-//    void getHistory() {
-//    }
-//
-//    @org.junit.jupiter.api.Test
-//    void saveWinners() {
-//    }
-//
-//    @org.junit.jupiter.api.Test
-//    void getWinners() {
-//    }
 }
